d314cd5019aade906747c66a164bacff3e2b6bac
Duduling <duduling20@gmail.com>
2022-08-10 12:45:57 +0900

    [inflearn] docs: Ch.01 - 01.세 수 중 최솟값

---
+++ inflearn-algorithm.js
@@ -0,0 +1,10 @@
+// #region - Ch.01 - 01.세 수 중 최솟값
+// function solution(a, b, c) {
+//   let answer = a;
+
+//   if (answer > b) answer = b;
+//   if (answer > c) answer = c;
+
+//   return answer;
+// }
+// #endregion
