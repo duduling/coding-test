88023b5a4b155bce6a3c46c781bb1ece067a2ba1
Duduling <duduling20@gmail.com>
2022-08-24 10:00:46 +0900

    [inflearn] docs: Ch.07 - 07. 좌표 정렬

--- inflearn-algorithm.ts
+++ inflearn-algorithm.ts
@@ -1032,8 +1032,19 @@
 //#endregion
 
 // #region - Ch.07 - 07. 좌표 정렬
-// function solution(){}
-// console.log(solution())
+function solution(arr: Array<Array<number>>): Array<Array<number>> {
+  arr.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+  return arr;
+}
+console.log(
+  solution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
 // #endregion
 
 // #region - Ch.07 - 08. 회의실 배정
