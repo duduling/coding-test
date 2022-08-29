4af1456d6e8e30034d6951b9689c3bef58091ab4
Duduling <duduling20@gmail.com>
2022-08-29 16:30:24 +0900

    [inflearn] docs: Ch.10 - 01. 계단오르기

--- inflearn-algorithm.ts
+++ inflearn-algorithm.ts
@@ -1744,10 +1744,17 @@
 // #region - Ch.10 - Dynamic programming(동적계획법 )
 
 // #region - Ch.10 - 01. 계단오르기
-// const solution = () => {
+// const solution = (n: number): number => {
+//   const dy = Array.from({ length: n + 1 }, () => 0);
+//   dy[1] = 1;
+//   dy[2] = 2;
 
-// }
-// console.log(solution())
+//   for (let i = 3; i <= n; i++) {
+//     dy[i] = dy[i - 2] + dy[i - 1];
+//   }
+
+//   return dy[n];
+// };
 // #endregion
 
 // #region - Ch.10 - 02. 돌다리 건너기
