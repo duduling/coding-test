976d7d9984668443ab81a1e6699cbac34a67fcff
Duduling <duduling20@gmail.com>
2022-09-01 20:07:34 +0900

    [programmers] docs: Lv1 - x만큼 간격이 있는 n개의 숫자

--- programmers.js
+++ programmers.js
@@ -1,6 +1,6 @@
 // #regioin - Level 1
 
-// #regioin - Lv1.직사각형 별찍기
+// #regioin - Lv1 - 직사각형 별찍기
 // process.stdin.setEncoding("utf8");
 // process.stdin.on("data", (data) => {
 //   const n = data.split(" ");
@@ -10,6 +10,12 @@
 //   const starts = "*".repeat(a);
 //   for (let i = 0; i < b; i++) console.log(starts);
 // });
+// #endregion
+
+// #regioin - Lv1 - x만큼 간격이 있는 n개의 숫자
+// function solution(x, n) {
+//   return Array.from({ length: n }, (_, i) => (i + 1) * x);
+// }
 // #endregion
 
 // #endregion
