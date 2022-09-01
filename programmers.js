0d669ef1e8c8bcef79c4972c5c821d8a5e9aa6b8
Duduling <duduling20@gmail.com>
2022-09-01 20:09:36 +0900

    [programmers] docs: Lv1 - 핸드폰 번호 가리기

--- programmers.js
+++ programmers.js
@@ -26,4 +26,10 @@
 // }
 // #endregion
 
+// #regioin - Lv1 - 핸드폰 번호 가리기
+// function solution(phone_number) {
+//   return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+// }
+// #endregion
+
 // #endregion
