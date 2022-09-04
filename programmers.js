ba8978994f40f704540d373587a831b2565da102
Duduling <duduling20@gmail.com>
2022-09-04 23:28:34 +0900

    [programmers] docs: Lv1 - 제일 작은 수 제거하기

--- programmers.js
+++ programmers.js
@@ -83,4 +83,13 @@
 // }
 // #endregion
 
+// #regioin - Lv1 - 제일 작은 수 제거하기
+// function solution(arr) {
+//   if (arr.length <= 1) return [-1];
+
+//   const minNum = Math.min(...arr);
+//   return arr.filter((num) => num !== minNum);
+// }
+// #endregion
+
 // #endregion
