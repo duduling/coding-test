91e96768fc23eb7f3d62f4a8fb3433b17a98d5d2
Duduling <duduling20@gmail.com>
2022-09-07 17:43:35 +0900

    [programmers] style: Clean code

--- programmers.js
+++ programmers.js
@@ -1,6 +1,6 @@
-// #regioin - Level 1
+// #region - Level 1
 
-// #regioin - Lv1 - 직사각형 별찍기
+// #region - Lv1 - 직사각형 별찍기
 // process.stdin.setEncoding("utf8");
 // process.stdin.on("data", (data) => {
 //   const n = data.split(" ");
@@ -12,13 +12,13 @@
 // });
 // #endregion
 
-// #regioin - Lv1 - x만큼 간격이 있는 n개의 숫자
+// #region - Lv1 - x만큼 간격이 있는 n개의 숫자
 // function solution(x, n) {
 //   return Array.from({ length: n }, (_, i) => (i + 1) * x);
 // }
 // #endregion
 
-// #regioin - Lv1 - 행렬의 덧셈
+// #region - Lv1 - 행렬의 덧셈
 // function solution(arr1, arr2) {
 //   return arr1.map((list, xIdx) =>
 //     list.map((value, yIdx) => value + arr2[xIdx][yIdx])
@@ -26,25 +26,25 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 핸드폰 번호 가리기
+// #region - Lv1 - 핸드폰 번호 가리기
 // function solution(phone_number) {
 //   return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
 // }
 // #endregion
 
-// #regioin - Lv1 - 하샤드 수
+// #region - Lv1 - 하샤드 수
 // function solution(x) {
 //   return !(x % `${x}`.split("").reduce((acc, cur) => acc + Number(cur), 0));
 // }
 // #endregion
 
-// #regioin - Lv1 - 평균 구하기
+// #region - Lv1 - 평균 구하기
 // function solution(arr) {
 //   return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
 // }
 // #endregion
 
-// #regioin - Lv1 - 콜라츠 추측
+// #region - Lv1 - 콜라츠 추측
 // function solution(num, cnt = 0) {
 //   if (num === 1) return 500 <= cnt ? -1 : cnt;
 
@@ -52,7 +52,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 최대공약수와 최소공배수
+// #region - Lv1 - 최대공약수와 최소공배수
 // function gcd(a, b) {
 //   if (b === 0) return a;
 
@@ -74,16 +74,15 @@
 // - a,b 를 서로 나눌때, 나누어진다면 b가 최대 공약수 이다. (a > b)
 // - 만약 a,b가 나누어지지 않으면 b와 a를 b로 나눈 나머지를 다시 나눈다
 // - 서로가 나누어지면 a%b 가 최대공약수이다. 나누어지지 않는다면 위처럼 b와 a를 b를 나눈 나머지를 다시 나눈다.
-
 // #endregion
 
-// #regioin - Lv1 - 짝수와 홀수
+// #region - Lv1 - 짝수와 홀수
 // function solution(num) {
 //   return num % 2 ? "Odd" : "Even";
 // }
 // #endregion
 
-// #regioin - Lv1 - 제일 작은 수 제거하기
+// #region - Lv1 - 제일 작은 수 제거하기
 // function solution(arr) {
 //   if (arr.length <= 1) return [-1];
 
@@ -92,14 +91,14 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 정수 제곱근 판별
+// #region - Lv1 - 정수 제곱근 판별
 // function solution(n) {
 //   const tmp = Math.sqrt(n);
 //   return Number.isInteger(tmp) ? Math.pow(tmp + 1, 2) : -1;
 // }
 // #endregion
 
-// #regioin - Lv1 - 정수 내림차순으로 배치하기
+// #region - Lv1 - 정수 내림차순으로 배치하기
 // function solution(n) {
 //   return Number(
 //     `${n}`
@@ -110,7 +109,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 자연수 뒤집어 배열로 만들기
+// #region - Lv1 - 자연수 뒤집어 배열로 만들기
 // function solution(n) {
 //   return `${n}`
 //     .split("")
@@ -119,22 +118,13 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 자연수 뒤집어 배열로 만들기
-// function solution(n) {
-//   return `${n}`
-//     .split("")
-//     .reverse()
-//     .map((str) => Number(str));
-// }
-// #endregion
-
-// #regioin - Lv1 - 자릿수 더하기
+// #region - Lv1 - 자릿수 더하기
 // function solution(n) {
 //   return `${n}`.split("").reduce((acc, cur) => acc + Number(cur), 0);
 // }
 // #endregion
 
-// #regioin - Lv1 - 이상한 문자 만들기
+// #region - Lv1 - 이상한 문자 만들기
 // function solution(s) {
 //   return s
 //     .toLowerCase()
@@ -151,7 +141,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 약수의 합
+// #region - Lv1 - 약수의 합
 // function solution(n) {
 //   let answer = 0;
 
@@ -163,7 +153,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 시저 암호
+// #region - Lv1 - 시저 암호
 // function solution(s, n) {
 //   let answer = "";
 
@@ -181,19 +171,19 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 문자열을 정수로 바꾸기
+// #region - Lv1 - 문자열을 정수로 바꾸기
 // function solution(s) {
 //   return Number(s);
 // }
 // #endregion
 
-// #regioin - Lv1 - 수박수박수박수박수박수?
+// #region - Lv1 - 수박수박수박수박수박수?
 // function solution(n) {
 //   return "수박".repeat(n / 2) + (n % 2 ? "수" : "");
 // }
 // #endregion
 
-// #regioin - Lv1 - 소수 찾기
+// #region - Lv1 - 소수 찾기
 // function solution(n) {
 //   const answer = new Set();
 
@@ -211,19 +201,19 @@
 // 소인수분해 활용
 // #endregion
 
-// #regioin - Lv1 - 서울에서 김서방 찾기
+// #region - Lv1 - 서울에서 김서방 찾기
 // function solution(seoul) {
 //   return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 // }
 // #endregion
 
-// #regioin - Lv1 - 문자열 다루기 기본
+// #region - Lv1 - 문자열 다루기 기본
 // function solution(s) {
 //   return s.length === 4 || s.length === 6 ? !/[a-z]/g.test(s) : false;
 // }
 // #endregion
 
-// #regioin - Lv1 - 문자열 내림차순으로 배치하기
+// #region - Lv1 - 문자열 내림차순으로 배치하기
 // function solution(s) {
 //   return s
 //     .split("")
@@ -232,7 +222,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 문자열 내 p와 y의 개수
+// #region - Lv1 - 문자열 내 p와 y의 개수
 // function solution(s) {
 //   const answer = {
 //     p: 0,
@@ -247,7 +237,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 문자열 내 마음대로 정렬하기
+// #region - Lv1 - 문자열 내 마음대로 정렬하기
 // function solution(strings, n) {
 //   return strings.sort((a, b) =>
 //     a[n] === b[n] ? (a > b ? 1 : -1) : a[n] > b[n] ? 1 : -1
@@ -262,13 +252,13 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 두 정수 사이의 합
+// #region - Lv1 - 두 정수 사이의 합
 // function solution(a, b) {
 //   return ((a + b) * (Math.abs(b - a) + 1)) / 2;
 // }
 // #endregion
 
-// #regioin - Lv1 - 나누어 떨어지는 숫자 배열
+// #region - Lv1 - 나누어 떨어지는 숫자 배열
 // function solution(arr, divisor) {
 //   const answer = arr.filter((num) => num % divisor === 0).sort((a, b) => a - b);
 
@@ -276,62 +266,11 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 상위 n개 레코드
+// #region - Lv1 - 상위 n개 레코드
 // SELECT NAME from ANIMAL_INS ORDER BY DATETIME ASC LIMIT 1
 // #endregion
 
-// #regioin - Lv1 - 여러 기준으로 정렬하기
-// SELECT ANIMAL_ID, NAME, DATETIME from ANIMAL_INS ORDER BY NAME ASC, DATETIME DESC
-// #endregion
-
-// #regioin - Lv1 - 가운데 글자 가져오기
-// function solution(s) {
-//   const targerIdx = Math.floor(s.length / 2);
-
-//   return s.length % 2 ? s[targerIdx] : s[targerIdx - 1] + s[targerIdx];
-// }
-// #endregion
-
-// #regioin - Lv1 - 부족한 금액 계산하기
-// function solution(price, money, count) {
-//   return Math.max(((price + price * count) * count) / 2 - money, 0);
-// }
-// #endregion
-
-// #regioin - Lv1 - 동물의 아이디와 이름
-// SELECT ANIMAL_ID, NAME from ANIMAL_INS ORDER BY ANIMAL_ID ASC
-// #endregion
-
-// #regioin - Lv1 - 나머지가 1이 되는 수 찾기
-// function solution(n) {
-//   for (let i = 2; i < n; i++) {
-//     if (n % i === 1) return i;
-//   }
-// }
-// #endregion
-
-// #regioin - Lv1 - 어린 동물 찾기
-// SELECT ANIMAL_ID, NAME from ANIMAL_INS WHERE INTAKE_CONDITION != "Aged" ORDER BY ANIMAL_ID ASC
-// #endregion
-
-// #regioin - Lv1 - [1차] 비밀지도
-// function solution(n, arr1, arr2) {
-//   const answer = [];
-
-//   for (let i = 0; i < n; i++) {
-//     answer.push(
-//       (arr1[i] | arr2[i])
-//         .toString(2)
-//         .padStart(n, "0")
-//         .replace(/0|1/g, (char) => (char === "1" ? "#" : " "))
-//     );
-//   }
-
-//   return answer;
-// }
-// #endregion
-
-// #regioin - Lv1 - [1차] 다트 게임
+// #region - Lv1 - [1차] 다트 게임
 // function solution(dartResult) {
 //   const OPTION_TABLE = {
 //     S: 1,
@@ -361,11 +300,94 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 역순 정렬하기
+// #region - Lv1 - 여러 기준으로 정렬하기
+// SELECT ANIMAL_ID, NAME, DATETIME from ANIMAL_INS ORDER BY NAME ASC, DATETIME DESC
+// #endregion
+
+// #region - Lv1 - 가운데 글자 가져오기
+// function solution(s) {
+//   const targerIdx = Math.floor(s.length / 2);
+
+//   return s.length % 2 ? s[targerIdx] : s[targerIdx - 1] + s[targerIdx];
+// }
+// #endregion
+
+// #region - Lv1 - [1차] 비밀지도
+// function solution(n, arr1, arr2) {
+//   const answer = [];
+
+//   for (let i = 0; i < n; i++) {
+//     answer.push(
+//       (arr1[i] | arr2[i])
+//         .toString(2)
+//         .padStart(n, "0")
+//         .replace(/0|1/g, (char) => (char === "1" ? "#" : " "))
+//     );
+//   }
+
+//   return answer;
+// }
+// #endregion
+
+// #region - Lv1 - 부족한 금액 계산하기
+// function solution(price, money, count) {
+//   return Math.max(((price + price * count) * count) / 2 - money, 0);
+// }
+// #endregion
+
+// #region - Lv1 - 동물의 아이디와 이름
+// SELECT ANIMAL_ID, NAME from ANIMAL_INS ORDER BY ANIMAL_ID ASC
+// #endregion
+
+// #region - Lv1 - 나머지가 1이 되는 수 찾기
+// function solution(n) {
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 1) return i;
+//   }
+// }
+// #endregion
+
+// #region - Lv1 - 어린 동물 찾기
+// SELECT ANIMAL_ID, NAME from ANIMAL_INS WHERE INTAKE_CONDITION != "Aged" ORDER BY ANIMAL_ID ASC
+// #endregion
+
+// #region - Lv1 - 2016년
+// function solution(a, b) {
+//   const dayArr = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+//   return dayArr[new Date(2016, a, b).getDay()];
+// }
+// #endregion
+
+// #region - Lv1 - 아픈 동물 찾기
+// SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION = "Sick"
+// #endregion
+
+// #region - Lv1 - 두 개 뽑아서 더하기 (DFS)
+// function solution(numbers) {
+//   const answer = new Set();
+
+//   (function DFS(L, idx, sum) {
+//     if (L === 2) return answer.add(sum);
+
+//     for (let i = idx; i < numbers.length; i++) {
+//       DFS(L + 1, i + 1, sum + numbers[i]);
+//     }
+//   })(0, 0, 0);
+
+//   return [...answer].sort((a, b) => a - b);
+// }
+// #endregion
+
+// #region - Lv1 - 이름이 있는 동물의 아이디
+// SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NOT NULL
+// #endregion
+
+// #region - Lv1 - 역순 정렬하기
 // SELECT NAME, DATETIME from ANIMAL_INS ORDER BY ANIMAL_ID DESC
 // #endregion
 
-// #regioin - Lv1 - 예산
+// #region - Lv1 - 예산
 // function solution(d, budget) {
 //   let answer = 0;
 
@@ -380,22 +402,16 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 3진법 뒤집기
+// #region - Lv1 - 3진법 뒤집기
 // function solution(n) {
-//   const tmp = n.toString(3);
-
-//   let answer = Number(tmp[0]);
-
-//   for (let i = 1; i < tmp.length; i++) {
-//     if (i === "0") continue;
-//     answer += Number(tmp[i]) * 3 ** i;
-//   }
-
-//   return answer;
+//   return n
+//     .toString(3)
+//     .split("")
+//     .reduce((acc, cur, idx) => acc + Number(cur) * 3 ** idx, 0);
 // }
 // #endregion
 
-// #regioin - Lv1 - 약수의 개수와 덧셈
+// #region - Lv1 - 약수의 개수와 덧셈
 // function solution(left, right) {
 //   let answer = 0;
 
@@ -408,7 +424,7 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 실패율
+// #region - Lv1 - 실패율
 // function solution(N, stages) {
 //   let accObj = stages.reduce((acc, cur) => {
 //     if (cur <= N) acc[cur] = (acc[cur] || 0) + 1;
@@ -429,16 +445,16 @@
 // }
 // #endregion
 
-// #regioin - Lv1 - 이름이 없는 동물의 아이디
+// #region - Lv1 - 이름이 없는 동물의 아이디
 // SELECT ANIMAL_ID from ANIMAL_INS WHERE NAME IS NULL
 
 // #endregion
-// #regioin - Lv1 - 최댓값 구하기
+
+// #region - Lv1 - 최댓값 구하기
 // SELECT DATETIME from ANIMAL_INS ORDER BY DATETIME DESC LIMIT 1
 // #endregion
 
-// #endregion
-// #regioin - Lv1 - 모든 레코드 조회하기
+// #region - Lv1 - 모든 레코드 조회하기
 // SELECT * FROM ANIMAL_INS
 // #endregion
 
