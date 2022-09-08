097fe48bbd39a5dc006468f7f2787bbaa36bdad5
Duduling <duduling20@gmail.com>
2022-09-08 16:01:59 +0900

    [programmers] docs: Lv1 - 같은 숫자는 싫어

--- programmers.js
+++ programmers.js
@@ -478,4 +478,17 @@
 //   return a.reduce((acc, cur, idx) => acc + cur * b[idx], 0);
 // }
 // #endregion
+
+// #region - Lv1 - 같은 숫자는 싫어
+// function solution(arr) {
+//   const answer = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+//   }
+
+//   return answer;
+// }
+// #endregion
+
 // #endregion
