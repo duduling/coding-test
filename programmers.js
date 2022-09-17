5a35ea4ec74a0dc6cd1a077fc4182b3e1bcb5a25
Duduling <duduling20@gmail.com>
2022-09-17 19:20:18 +0900

    [programmers] docs: Lv2 - 예상 대진표

--- programmers.js
+++ programmers.js
@@ -1036,5 +1036,18 @@
 // console.log(solution([2, 6, 8, 14]));
 // #endregion
 
+// #region - Lv2 - 예상 대진표
+// function solution(n, a, b) {
+//   let answer = 0;
+
+//   while (a !== b) {
+//     a = Math.ceil(a / 2);
+//     b = Math.ceil(b / 2);
+//     answer++;
+//   }
+
+//   return answer;
+// }
+// #endregion
 // #region - Level 3 -
 // #endregion
