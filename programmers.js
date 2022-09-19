2380152dfe9f5de892aef1d878786e36cdaa014f
Duduling <duduling20@gmail.com>
2022-09-19 16:05:42 +0900

    [programmers] docs: Lv2 - 행렬의 곱셈

--- programmers.js
+++ programmers.js
@@ -1082,5 +1082,24 @@
 // }
 // #endregion
 
+// #region - Lv2 - 행렬의 곱셈
+// function solution(arr1, arr2) {
+//   const answer = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     let tmpRow = [];
+//     for (let j = 0; j < arr2[0].length; j++) {
+//       let tmp = 0;
+//       for (let k = 0; k < arr2.length; k++) {
+//         tmp += arr1[i][k] * arr2[k][j];
+//       }
+//       tmpRow.push(tmp);
+//     }
+//     answer.push(tmpRow);
+//   }
+//   return answer;
+// }
+// 행렬 곱하기 -> https://www.youtube.com/watch?v=JpSe38UHaos
+// #endregion
+
 // #region - Level 3 -
 // #endregion
