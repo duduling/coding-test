44fb46338de74bbc1fdfe1ea5db9e77930f10dfd
Duduling <duduling20@gmail.com>
2022-09-28 23:01:23 +0900

    [baekjoon] 10718: We love kriii

---
+++ src/baekjoon/10718/index.ts
@@ -0,0 +1,17 @@
+// 테스트용
+// const input = require("fs")
+//   .readFileSync("ex.txt")
+//   .toString()
+//   .split(/\r\n|\r|\n/);
+
+// 제출용
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split(/\r\n|\r|\n/);
+
+// const solution = () => {
+//   for (let i = 0; i < 2; i++) console.log("강한친구 대한육군");
+// };
+
+// solution();
--- src/baekjoon/2257/index.ts
+++ src/baekjoon/2257/index.ts
@@ -1,17 +1,17 @@
-// 테스트용
-const input = require("fs")
-  .readFileSync("ex.txt")
-  .toString()
-  .split(/\r\n|\r|\n/);
-
-// 제출용
+// // 테스트용
 // const input = require("fs")
-//   .readFileSync("/dev/stdin")
+//   .readFileSync("ex.txt")
 //   .toString()
 //   .split(/\r\n|\r|\n/);
 
-const solution = () => {
-  return "Hello World!";
-};
+// // 제출용
+// // const input = require("fs")
+// //   .readFileSync("/dev/stdin")
+// //   .toString()
+// //   .split(/\r\n|\r|\n/);
 
-console.log(solution());
+// const solution = () => {
+//   return "Hello World!";
+// };
+
+// console.log(solution());
