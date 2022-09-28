b57bc33f4e554ad811b0c4d81abffec2edc8639f
Duduling <duduling20@gmail.com>
2022-09-28 23:38:56 +0900

    [baekjoon] 1000: A+B

---
+++ src/baekjoon/1000/index.js
@@ -0,0 +1,15 @@
+// 테스트용
+// const input = require("fs")
+//   .readFileSync("ex.txt")
+//   .toString()
+//   .trim()
+//   .split(/\s/);
+
+// 제출용
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+// function solution([a, b]) {
+//   console.log(Number(a) + Number(b));
+// }
+
+// solution(input);
