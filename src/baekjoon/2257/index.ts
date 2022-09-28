f1672586adce04eccafd2fab7a5f5317536c0472
Duduling <duduling20@gmail.com>
2022-09-28 22:52:55 +0900

    [baekjoon] 2557: Hello World

---
+++ src/baekjoon/2257/index.ts
@@ -0,0 +1,17 @@
+// 테스트용
+const input = require("fs")
+  .readFileSync("ex.txt")
+  .toString()
+  .split(/\r\n|\r|\n/);
+
+// 제출용
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split(/\r\n|\r|\n/);
+
+const solution = () => {
+  return "Hello World!";
+};
+
+console.log(solution());
