44c1ddca3a23ab3848d7cc5c2aa6aea3abf5f1ac
Duduling <duduling20@gmail.com>
2022-09-30 23:59:01 +0900

    [baekjoon] docs: 1152 - 단어의 개수

---
+++ src/baekjoon/1152/index.js
@@ -0,0 +1,19 @@
+// // 테스트용
+// const input = require("fs")
+//   .readFileSync("ex.txt")
+//   .toString()
+//   .split(/\r\n|\r|\n/);
+
+// // 제출용
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split(/\r\n|\r|\n/);
+
+// const solution = (inputList) => {
+//   const [str] = inputList;
+
+//   console.log(str.split(" ").filter((word) => word).length);
+// };
+
+// solution(input);
