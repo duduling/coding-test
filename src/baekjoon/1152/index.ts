6fb3043e36bc96355aa98beda21097528fea0a5f
Duduling <duduling20@gmail.com>
2022-10-04 09:39:29 +0900

    [baekjoon] docs: 1152 - 단어의 개수 수정

---
+++ src/baekjoon/1152/index.ts
@@ -0,0 +1,21 @@
+// // 테스트용
+// const input = require("fs")
+//   .readFileSync("ex.txt")
+//   .toString()
+//   .split(" ")
+//   .map((val: string) => +val);
+
+// // 제출용
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split(" ")
+//   .map((val: string) => +val);
+
+// const solution = (inputList: number[]) => {
+//   const [a, b] = inputList;
+
+//   console.log(a === b ? "==" : a < b ? "<" : ">");
+// };
+
+// solution(input);
