f9cc65b2ea70c328e21346f852e97f9a9df77f44
Duduling <duduling20@gmail.com>
2022-10-04 09:40:52 +0900

    [baekjoon] docs: 2438 - 별 찍기 1

---
+++ src/baekjoon/2438/index.ts
@@ -0,0 +1,18 @@
+// 테스트용
+// const input = require("fs")
+//   .readFileSync("ex.txt")
+//   .toString()
+//   .trim()
+//   .split(/\s/);
+
+// 제출용
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+// function solution(input: any) {
+//   const n: number = +input[0];
+
+//   for (let i = 1; i <= n; i++) {
+//     console.log("*".repeat(i));
+//   }
+// }
+// solution(input);
