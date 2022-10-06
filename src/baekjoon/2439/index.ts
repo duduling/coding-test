a43445c6dbdcc4975400fbfc7fdaf0ce9616855b
Duduling <duduling20@gmail.com>
2022-10-06 18:59:54 +0900

    [baekjoon] 2439 - 별 찍기 2

---
+++ src/baekjoon/2439/index.ts
@@ -0,0 +1,17 @@
+// For Test
+// const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const n: number = +input[0];
+
+//   // Logic
+//   for (let i = 1; i <= n; i++) {
+//     console.log(" ".repeat(n - i) + "*".repeat(i));
+//   }
+// }
+// solution(input);
