9ebd93a70478995556229735875e0a6eab0f531e
Duduling <duduling20@gmail.com>
2022-10-08 00:08:27 +0900

    style: 주석 처리

--- src/baekjoon/2475/index.ts
+++ src/baekjoon/2475/index.ts
@@ -1,17 +1,17 @@
 // For Test
-const input = require("fs").readFileSync("./ex.txt").toString().split("\r\n"); // // [" " | "\r\n"]
+// const input = require("fs").readFileSync("./ex.txt").toString().split("\r\n"); // // [" " | "\r\n"]
 
 // For Submit
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n"); // [" " | "\n"]
 
 // My Code
-function solution(input: any) {
-  // Initial Setting
-  const numList: number[] = input[0].split(" ").map((val: string) => +val);
+// function solution(input: any) {
+//   // Initial Setting
+//   const numList: number[] = input[0].split(" ").map((val: string) => +val);
 
-  // Logic
-  const answer = numList.reduce((acc, cur) => acc + cur ** 2, 0) % 10;
+//   // Logic
+//   const answer = numList.reduce((acc, cur) => acc + cur ** 2, 0) % 10;
 
-  console.log(answer);
-}
-solution(input);
+//   console.log(answer);
+// }
+// solution(input);
