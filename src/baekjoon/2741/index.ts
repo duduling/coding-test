1784eef635465fb10810a45157fc6e348d4ca7bb
Duduling <duduling20@gmail.com>
2022-10-08 13:28:43 +0900

    [baekjoon] 2741 - N 찍기

---
+++ src/baekjoon/2741/index.ts
@@ -0,0 +1,19 @@
+// For Test
+// const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const n: number = +input[0];
+
+//   // Logic
+//   let answer = "";
+//   for (let i = 1; i <= n; i++) {
+//     answer += `${i}\n`;
+//   }
+//   console.log(answer);
+// }
+// solution(input);
