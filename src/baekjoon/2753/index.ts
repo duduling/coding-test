81dfc62bbb63643075751b087612a66fab17e3f1
Duduling <duduling20@gmail.com>
2022-10-11 23:53:54 +0900

    style: 주석 처리

--- src/baekjoon/2753/index.ts
+++ src/baekjoon/2753/index.ts
@@ -1,15 +1,15 @@
 // For Test
-const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
+// const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
 
 // For Submit
 // const input = require("fs").readFileSync("/dev/stdin").toString().split(" "); // [" " | "\n"]
 
 // My Code
-function solution(input: any) {
-  // Initial Setting
-  const year: number = +input[0];
+// function solution(input: any) {
+//   // Initial Setting
+//   const year: number = +input[0];
 
-  // Logic
-  console.log(year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0) ? 1 : 0);
-}
-solution(input);
+//   // Logic
+//   console.log(year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0) ? 1 : 0);
+// }
+// solution(input);
