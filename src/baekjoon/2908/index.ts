35f88a004836673112df579c32eb9ed8a6e59872
Duduling <duduling20@gmail.com>
2022-10-12 23:23:46 +0900

    [baekjoon] 2908 - 상수

---
+++ src/baekjoon/2908/index.ts
@@ -0,0 +1,19 @@
+// For Test
+// const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const a = input[0];
+//   const b = input[1];
+
+//   // Logic
+//   const reverseA: number = +String(a).split("").reverse().join("");
+//   const reverseB: number = +String(b).split("").reverse().join("");
+
+//   console.log(Math.max(reverseA, reverseB));
+// }
+// solution(input);
