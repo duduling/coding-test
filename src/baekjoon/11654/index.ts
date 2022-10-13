fa99d9822321b4c709addd8f27a761fc940c13c5
Duduling <duduling20@gmail.com>
2022-10-14 00:47:46 +0900

    [baekjoon] 11654 - 아스키 코드

---
+++ src/baekjoon/11654/index.ts
@@ -0,0 +1,20 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   //
+//   // Logic
+//   for (let i = 0; i < input.length; i++) {
+//     const [a, b]: number[] = input[i].split(" ").map((val: string) => +val);
+
+//     if (a === 0 && b === 0) return;
+
+//     console.log(a + b);
+//   }
+// }
+// solution(input);
