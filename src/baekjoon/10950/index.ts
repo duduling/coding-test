90b7ff30581ff9201b611f28871eb7b102d9e5f8
Duduling <duduling20@gmail.com>
2022-10-14 00:33:00 +0900

    [baekjoon] 10950 - A+B - A+B - 3

---
+++ src/baekjoon/10950/index.ts
@@ -0,0 +1,19 @@
+// For Test
+// const input = require("fs").readFileSync("./ex.txt").toString().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const n = input[0];
+
+//   // Logic
+//   for (let i = 1; i <= n; i++) {
+//     const [a, b]: number[] = input[i].split(" ").map((val: string) => +val);
+
+//     console.log(a + b);
+//   }
+// }
+// solution(input);
