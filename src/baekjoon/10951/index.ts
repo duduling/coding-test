cc6a422415b2701c632f50e25af64000a8a26f63
Duduling <duduling20@gmail.com>
2022-10-14 00:38:03 +0900

    [baekjoon] 10951 - A+B - A+B - 4

---
+++ src/baekjoon/10951/index.ts
@@ -0,0 +1,18 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   //
+//   // Logic
+//   for (let i = 0; i < input.length; i++) {
+//     const [a, b]: number[] = input[i].split(" ").map((val: string) => +val);
+
+//     console.log(a + b);
+//   }
+// }
+// solution(input);
