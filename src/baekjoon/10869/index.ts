7612139a0edcf5dcb9397c31dd2c4e718b358c07
Duduling <duduling20@gmail.com>
2022-10-14 00:25:33 +0900

    [baekjoon] 10869 - 사칙연산

---
+++ src/baekjoon/10869/index.ts
@@ -0,0 +1,20 @@
+// For Test
+// const input = require("fs").readFileSync("./ex.txt").toString().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = require("fs").readFileSync("/dev/stdin").toString().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const a: number = +input[0];
+//   const b: number = +input[1];
+
+//   // Logic
+//   console.log(a + b);
+//   console.log(a - b);
+//   console.log(a * b);
+//   console.log(Math.floor(a / b));
+//   console.log(a % b);
+// }
+// solution(input);
