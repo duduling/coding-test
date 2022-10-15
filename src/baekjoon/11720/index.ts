ba22c5b3ce144e0f98bc9aab3d2d43d8f9c3ef4b
Duduling <duduling20@gmail.com>
2022-10-15 20:42:31 +0900

    [baekjoon] 11720 - 숫자의 합

---
+++ src/baekjoon/11720/index.ts
@@ -0,0 +1,20 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const n = input[0];
+//   const numArr: string = input[1];
+
+//   // Logic
+//   let answer = 0;
+//   for (let i = 0; i < n; i++) {
+//     answer += +numArr[i];
+//   }
+//   console.log(answer);
+// }
+// solution(input);
