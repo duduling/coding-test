fdae6a622d7725c0334ae2066e11b881e4e126d3
Duduling <duduling20@gmail.com>
2022-10-16 12:34:32 +0900

    [baekjoon] 10172 - 개

---
+++ src/baekjoon/10172/index.ts
@@ -0,0 +1,14 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+function solution() {
+  // Initial Setting
+
+  // Logic
+  console.log('|\\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\\__|');
+}
+solution();
