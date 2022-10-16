0098f451289b0a1058632c3fc526e41905c7b52c
Duduling <duduling20@gmail.com>
2022-10-16 12:30:28 +0900

    [baekjoon] 10171 - 고양이

---
+++ src/baekjoon/10171/index.ts
@@ -0,0 +1,14 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+function solution() {
+  // Initial Setting
+
+  // Logic
+  console.log("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+}
+solution();
