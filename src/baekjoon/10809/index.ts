588f0f30bbd9d02d249bf52d262b8048ab9230f6
Duduling <duduling20@gmail.com>
2022-10-16 12:51:10 +0900

    [baekjoon] 10809 - 알파벳 찾기

---
+++ src/baekjoon/10809/index.ts
@@ -0,0 +1,25 @@
+// For Test
+const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+function solution(input: any) {
+  // Initial Setting
+  const word: string = input[0];
+
+  // Logic
+  const answer = Array(26).fill(-1);
+
+  for (let i = 0; i < word.length; i++) {
+    const convertAscii: number = word.charCodeAt(i) - 97;
+
+    if (answer[convertAscii] === -1) {
+      answer[convertAscii] = i;
+    }
+  }
+
+  console.log(answer);
+}
+solution(input);
