b53fa8e0ca4743114744b7f064375a8f2b20d6bc
Duduling <duduling20@gmail.com>
2022-10-19 19:06:15 +0900

    [baekjoon] 10998 - AxB

---
+++ src/baekjoon/10998/index.ts
@@ -0,0 +1,15 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const [a, b]: string[] = input;
+
+//   // Logic
+//   console.log(+a * +b);
+// }
+// solution(input);
