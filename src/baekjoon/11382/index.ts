31d33527151402cf3eac6442b678ba9aa390643e
Duduling <duduling20@gmail.com>
2022-10-19 22:58:47 +0900

    [baekjoon] 11382 - 꼬마 정민

---
+++ src/baekjoon/11382/index.ts
@@ -0,0 +1,15 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const [a, b, c]: number[] = input.map((val: string) => +val);
+
+//   // Logic
+//   console.log(a + b + c);
+// }
+// solution(input);
