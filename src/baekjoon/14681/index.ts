4697550701acf6ba6cd8d1b75bcb9c4100e8ad57
Duduling <duduling20@gmail.com>
2022-10-20 21:00:31 +0900

    [baekjoon] 14681 - 사분면 고르기

---
+++ src/baekjoon/14681/index.ts
@@ -0,0 +1,28 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const x: number = +input[0];
+//   const y: number = +input[1];
+
+//   // Logic
+//   if (0 < x) {
+//     if (0 < y) {
+//       console.log(1);
+//     } else {
+//       console.log(4);
+//     }
+//   } else {
+//     if (0 < y) {
+//       console.log(2);
+//     } else {
+//       console.log(3);
+//     }
+//   }
+// }
+// solution(input);
