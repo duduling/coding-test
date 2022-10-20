84c972e007b94645b821c51cf97312c2d8bd24a8
Duduling <duduling20@gmail.com>
2022-10-20 21:21:39 +0900

    [baekjoon] 10872 - 팩토리얼

---
+++ src/baekjoon/10872/index.ts
@@ -0,0 +1,21 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+// const factorialFunc = (n: number): number => {
+//   if (n === 0) return 1;
+
+//   return n * factorialFunc(n - 1);
+// };
+
+// function solution(input: any) {
+//   // Initial Setting
+//   const n = +input[0];
+
+//   // Logic
+//   console.log(factorialFunc(n));
+// }
+// solution(input);
