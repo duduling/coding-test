38f7adf7027ec313af8470a0791b16d3a380e1dd
Duduling <duduling20@gmail.com>
2022-10-20 21:15:50 +0900

    [baekjoon] 2420 - 사파리월드

---
+++ src/baekjoon/2420/index.ts
@@ -0,0 +1,16 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: any) {
+//   // Initial Setting
+//   const a: number = +input[0];
+//   const b: number = +input[1];
+
+//   // Logic
+//   console.log(Math.abs(a - b));
+// }
+// solution(input);
