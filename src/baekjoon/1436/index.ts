383ba68ec4136a2269c36dcfba4faf022bcdee4a
Duduling <duduling20@gmail.com>
2022-11-01 23:47:19 +0900

    style: 주석 처리

--- src/baekjoon/1436/index.ts
+++ src/baekjoon/1436/index.ts
@@ -1,24 +1,24 @@
 // For Test
-const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
 
 // For Submit
 // const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
 
 // My Code
-function solution(input: string[]) {
-  // Initial Setting
-  const n: number = +input[0];
+// function solution(input: string[]) {
+//   // Initial Setting
+//   const n: number = +input[0];
 
-  // Logic
-  let cnt: number = 1;
-  let titleNum: number = 666;
-  while (cnt < n) {
-    titleNum++;
+//   // Logic
+//   let cnt: number = 1;
+//   let titleNum: number = 666;
+//   while (cnt < n) {
+//     titleNum++;
 
-    if (`${titleNum}`.includes("666")) {
-      cnt++;
-    }
-  }
-  console.log(titleNum);
-}
-solution(input);
+//     if (`${titleNum}`.includes("666")) {
+//       cnt++;
+//     }
+//   }
+//   console.log(titleNum);
+// }
+// solution(input);
