119c29ac72787ac2c4a9230d8ce1bdf3f4ab73ca
Duduling <duduling20@gmail.com>
2022-11-07 23:12:24 +0900

    style: 주석 처리

--- src/baekjoon/2744/index.ts
+++ src/baekjoon/2744/index.ts
@@ -1,27 +1,27 @@
 // For Test
-const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split(" "); // // [" " | "\r\n"]
 
 // For Submit
 // const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
 
 // My Code
-function solution(input: string[]) {
-  // Initial Setting
-  const word = input[0];
+// function solution(input: string[]) {
+//   // Initial Setting
+//   const word = input[0];
 
-  // Logic
-  let answer: string = "";
-  const REVERSE_CHAR_NUM = 32;
+//   // Logic
+//   let answer: string = "";
+//   const REVERSE_CHAR_NUM = 32;
 
-  for (let i = 0; i < word.length; i++) {
-    const charCode = word.charCodeAt(i);
+//   for (let i = 0; i < word.length; i++) {
+//     const charCode = word.charCodeAt(i);
 
-    if (charCode < 97) {
-      answer += String.fromCharCode(charCode + REVERSE_CHAR_NUM);
-    } else {
-      answer += String.fromCharCode(charCode - REVERSE_CHAR_NUM);
-    }
-  }
-  console.log(answer);
-}
-solution(input);
+//     if (charCode < 97) {
+//       answer += String.fromCharCode(charCode + REVERSE_CHAR_NUM);
+//     } else {
+//       answer += String.fromCharCode(charCode - REVERSE_CHAR_NUM);
+//     }
+//   }
+//   console.log(answer);
+// }
+// solution(input);
