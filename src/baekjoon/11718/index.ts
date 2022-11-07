23f549604e1f82f8c9b13f5b3eee3218054d4e04
Duduling <duduling20@gmail.com>
2022-11-07 23:11:35 +0900

    [baekjoon] 11718 - 그대로 출력하기

---
+++ src/baekjoon/11718/index.ts
@@ -0,0 +1,22 @@
+// For Test
+// const input = `${require("fs").readFileSync("./ex.txt")}`.trim().split("\r\n"); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // [" " | "\n"]
+
+// My Code
+// function solution(input: string[]) {
+//   // Initial Setting
+//   const n = input.length;
+
+//   // Logic
+
+//   let answer: string = "";
+
+//   for (let i = 0; i < n; i += 1) {
+//     answer += `${input[i]}\n`;
+//   }
+
+//   console.log(answer.trim());
+// }
+// solution(input);
