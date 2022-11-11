74a4f717ad1c829d29e028b867e5eb4e39f7cd6d
Duduling <duduling20@gmail.com>
2022-11-11 09:16:51 +0900

    [baekjoon] 15964 - 이상한 기호

---
+++ src/baekjoon/15964/index.ts
@@ -0,0 +1,16 @@
+// For Test
+// const TXT = `4 3`;
+// const input = TXT.trim().split(" "); // // [" " | "\r\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split(" "); // [" " | "\n"]
+
+// My Code
+// function solution(input: string[]) {
+//   // Initial Setting
+//   const [a, b]: number[] = input.map((val: string) => +val);
+
+//   // Logic
+//   console.log(a ** 2 - b ** 2);
+// }
+// solution(input);
