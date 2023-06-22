1f96727c595c548e5668740b92e767ce3963af75
duduling <duduling20@gmail.com>
2023-06-22 16:47:20 +0900

    [leetcode] 2637 - Debounce

---
+++ src/leetcoce/medium/2637 - Debounce.js
@@ -0,0 +1,18 @@
+const debounce = (fn, t) => {
+  let timer = null;
+
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, t);
+  };
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
