7d0485f0c83a6dacc7528215a037070d1b49a869
duduling <duduling20@gmail.com>
2023-07-19 23:11:47 +0900

    [baekjoon] 1655 - 가운데를 말해요

---
+++ src/baekjoon/1655/index.ts
@@ -0,0 +1,15 @@
+// For Test with Example
+export const input = ``.trim().split("\n"); // [" " | "\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // " " | "\n"]
+
+// My Code
+const solution = (input: string[]) => {
+  // Initial Setting
+  const n = input[0];
+
+  // Logic
+  for (let i = 1; i <= input.length; i += 1) {}
+};
+console.log(solution(input));
