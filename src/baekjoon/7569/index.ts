cc3f168e79dd90d0f1cd0f4a505b0be30d2cee99
duduling <duduling20@gmail.com>
2024-05-12 20:11:42 +0900

    [baekjoon] 7569 - 토마토

--- src/baekjoon/7569/index.ts
+++ src/baekjoon/7569/index.ts
@@ -1,11 +1,11 @@
 // For Test with Example
-export const input = `4 3 2
-1 1 1 1
-1 1 1 1
-1 1 1 1
-1 1 1 1
--1 -1 -1 -1
-1 1 1 -1`
+export const input = `3 3 2
+0 0 1
+0 -1 0
+1 0 0
+0 1 0
+-1 0 0 
+0 0 0`
   .trim()
   .split("\n"); // [" " | "\n"]
 
@@ -13,57 +13,62 @@ export const input = `4 3 2
 // const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // " " | "\n"]
 
 // My Code
-const FOUR_WAYS: number[][] = [
-  [1, 0],
-  [0, 1],
-  [0, -1],
-  [-1, 0],
-];
-const solution = ([MN, ...input]: string[]) => {
+const solution = (input: string[]) => {
   // Initial Setting
-  const [M, N]: number[] = MN.split(" ").map(Number);
+  const [M, N, H]: number[] = input.shift().split(" ").map(Number);
+  const DIRECTION_Z = [0, 0, 0, 0, -1, 1];
+  const DIRECTION_Y = [0, 0, -1, 1, 0, 0];
+  const DIRECTION_X = [-1, 1, 0, 0, 0, 0];
 
-  // Logic
-  const tomatoFarm = input.map((str: string) => str.split(" ").map(Number));
-  const queue: number[][] = [];
+  const queue = [];
+  const boxes = Array.from(Array(H), () =>
+    Array.from(Array(N), () => Array.from(Array(M).fill(0)))
+  );
 
-  let answer: number = 0;
-  let pointerIdx: number = 0;
-  let zeroCount: number = 0;
+  for (let z = 0; z < H; z++) {
+    for (let y = 0; y < N; y++) {
+      boxes[z][y] = input[z * N + y].split(" ").map(Number);
+    }
+  }
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      switch (tomatoFarm[i][j]) {
-        case 1:
-          queue.push([j, i, 0]);
-          break;
-        case 0:
-          zeroCount++;
-          break;
+  let unripeTomato = 0;
+  for (let z = 0; z < H; z++) {
+    for (let y = 0; y < N; y++) {
+      for (let x = 0; x < M; x++) {
+        if (boxes[z][y][x] == 0) unripeTomato++;
 
-        default:
-          break;
+        if (boxes[z][y][x] == 1) queue.push([z, y, x, 0]);
       }
     }
   }
 
-  while (pointerIdx < queue.length) {
-    const [x, y, count]: number[] = queue[pointerIdx++];
+  let queuePointer = 0;
+  let answer = 0;
+  while (queue.length > queuePointer) {
+    const [z, y, x, days]: number[] = queue[queuePointer++];
 
-    FOUR_WAYS.forEach(([offsetX, offsetY]: number[]) => {
-      const moveX: number = x + offsetX;
-      const moveY: number = y + offsetY;
+    for (let i = 0; i < 6; i++) {
+      const moveZ: number = z + DIRECTION_Z[i];
+      const moveY: number = y + DIRECTION_X[i];
+      const moveX: number = x + DIRECTION_Y[i];
 
-      if (tomatoFarm?.[moveY]?.[moveX] === 0) {
-        queue.push([moveX, moveY, count + 1]);
-        zeroCount--;
-
-        tomatoFarm[moveY][moveX] = 1;
-        answer = Math.max(answer, count + 1);
+      if (
+        0 <= moveZ &&
+        moveZ < H &&
+        0 <= moveY &&
+        moveY < N &&
+        0 <= moveX &&
+        moveX < M &&
+        !boxes[moveZ][moveY][moveX]
+      ) {
+        boxes[moveZ][moveY][moveX] = 1;
+        queue.push([moveZ, moveY, moveX, days + 1]);
+        unripeTomato -= 1;
       }
-    });
+    }
+    answer = days;
   }
 
-  return zeroCount ? -1 : answer;
+  return unripeTomato ? -1 : answer;
 };
 console.log(solution(input));
