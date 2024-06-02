53127155f9b7e70dce6ae49583bad6e4ed10c94f
duduling <duduling20@gmail.com>
2024-06-02 19:24:35 +0900

    [baekjoon] 9251 - LCS

---
+++ src/baekjoon/15686/index.ts
@@ -0,0 +1,14 @@
+// For Test with Example
+export const input = ``.trim().split("\n"); // [" " | "\n"]
+
+// For Submit
+// const input = `${require("fs").readFileSync("/dev/stdin")}`.trim().split("\n"); // " " | "\n"]
+
+// My Code
+const solution = (input: string[]) => {
+  // Initial Setting
+  const N = input.shift();
+
+  // Logic
+};
+console.log(solution(input));
